3772bbd37a21d09e7b9160c3319a4e646907a0ed
Austin <aatehilla@gmail.com>
2020-03-11 11:23:29 +0100

    services component created and page populated

---
+++ src/components/ind_dashboard_components/services-components/service-cards/easservice.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import { Card, Button, Badge } from 'react-bootstrap';
+
+
+
+
+const EASService = () => {
+
+    const [badge, setBadge] = useState(true);
+
+    return (
+        <div>
+            <Card style={{ width: '16rem', height: '18rem' }}>
+                <Card.Body>
+                    {badge ? <Badge variant="success" style={{ float: 'right' }}>New 9</Badge> : ""}
+                    <Card.Title>Eduction Advisory Service</Card.Title>
+                    <Card.Text>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.incididunt ut labore et dolore magna aliqua. 
+                    </Card.Text>
+                    <Button
+                        variant="primary"
+                        size="sm"
+                        className="btn btn-info btn-sm"
+                        // style={{ background: '#97ba0d', borderRadius: 20, border: '#97ba0d' }}
+                    >
+                        More Info
+                    </Button>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+}
+
+export default EASService;
--- src/components/ind_dashboard_components/services.js
+++ src/components/ind_dashboard_components/services.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardDeck } from 'react-bootstrap';
+import { Spinner } from 'react-bootstrap';
 import EASCard from './services-components/cards/eascard';
 import EFACard from './services-components/cards/efacard';
 import LMCard from './services-components/cards/lmcard';
 import RMCard from './services-components/cards/rmcard';
+import EASService from './services-components/service-cards/easservice';
+import EFAService from './services-components/service-cards/efaservice';
+import LMService from './services-components/service-cards/lmservice';
+import RMService from './services-components/service-cards/rmservice';
 import axios from 'axios';
 
 
@@ -11,10 +15,11 @@ import axios from 'axios';
 
 export default function Services() {
 
-    const [EAS, setEAS] = useState(false);
-    const [EFA, setEFA] = useState(false);
-    const [RM, setRM] = useState(false);
-    const [LM, setLM] = useState(false);
+
+    const [pageEFA, setPageEFA] = useState(0);
+    const [pageRM, setPageRM] = useState(0);
+    const [pageLM, setPageLM] = useState(0);
+    const [pageEAS, setPageEAS] = useState(0);
 
 
     useEffect(() => {
@@ -26,55 +31,49 @@ export default function Services() {
         //eas
         axios.get(`http://localhost:5000/subscriptioneas/${userData.id}`)
             .then(res => {
-                if (res.data[0].sub_status == true) {
-                    setEAS(true)
-                }
+                res.data[0].sub_status ? setPageEAS(1) : setPageEAS(2)
             }).catch(err => console.log(err));
 
         //efa
         axios.get(`http://localhost:5000/subscriptionefa/${userData.id}`)
             .then(res => {
-                if (res.data[0].sub_status == true) {
-                    setEFA(true);
-                }
+                res.data[0].sub_status ? setPageEFA(1) : setPageEFA(2)
             })
             .catch(err => console.log(err))
 
         //lm
         axios.get(`http://localhost:5000/subscriptionlm/${userData.id}`)
             .then(res => {
-                if (res.data[0].sub_status == true) {
-                    setLM(true);
-                }
-            }).catch(err => console.log(err))
+                res.data[0].sub_status ? setPageLM(1) : setPageLM(2);
+            })
+            .catch(err => console.log(err))
 
         //rm
         axios.get(`http://localhost:5000/subscriptionrm/${userData.id}`)
             .then(res => {
-                if (res.data[0].sub_status == true) {
-                    setLM(true);
-                }
-            }).catch(err => console.log(err))
+                res.data[0].sub_status ? setPageRM(1) : setPageRM(2)
+            })
+            .catch(err => console.log(err))
     })
     return (
         <div className="container">
             <div className="row">
                 <div className="col col-lg-3 col-sm-6">
                     <br />
-                    {EAS ? "EASserviceComponent" : <EASCard />}
+                    {(pageEAS === 0) ? <Spinner as="span" animation="grow" role="status" aria-hidden="true" /> : (pageEAS === 1) ? <EASService /> : (pageEAS === 2) ? <EASCard /> : ""}
                 </div>
                 <div className="col col-lg-3 col-sm-6" >
                     <br />
-                    {EFA ? 'EFAserviceComponent' : <EFACard />}
+                    {(pageEFA === 0) ? <Spinner as="span" animation="grow" size="lg" role="status" aria-hidden="true" /> : (pageEFA === 1) ? <EFAService /> : (pageEFA === 2) ? <EFACard /> : ""}
                 </div>
 
                 <div className="col col-lg-3 col-sm-6" >
                     <br />
-                    {LM ? "LMserviceComponent" : <LMCard />}
+                    {(pageLM === 0) ? <Spinner as="span" animation="grow" size="lg" role="status" aria-hidden="true" /> : (pageLM === 1) ? <LMService /> : (pageLM === 2) ? <LMCard /> : ""}
                 </div>
                 <div className="col col-lg-3 col-sm-6" >
                     <br />
-                    {RM ? "RMserviceComponent" : <RMCard />}
+                    {(pageRM === 0) ? <Spinner as="span" animation="grow" size="lg" role="status" aria-hidden="true" /> : (pageRM === 1) ? <RMService /> : (pageRM === 2) ? <RMCard /> : ""}
                 </div>
 
             </div>
